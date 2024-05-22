a6adc79ae487dbf2bd64d02134e78312d2e12d6b
bxljoy <259500313@qq.com>
2024-05-22 02:41:29 +0200

    refactor: Add video background to SideNav component

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -3,7 +3,19 @@ import { dancingScript } from "@/styles/fonts";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col bg-blue-600 md:px-2">
+    <div className="flex h-full flex-col md:px-2">
+      <div className="fixed left-0 top-0 z-[-10] w-96 h-screen">
+        <video
+          playsInline
+          autoPlay
+          muted
+          loop
+          className="object-cover w-full h-full"
+        >
+          <source src="assets/videos/background.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <Link className="m-6" href="/">
         <div className={`${dancingScript.className} text-white text-3xl`}>
           Share Notes
