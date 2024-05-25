23cb81d9891828716235a9b72b4b725a02af9a3b
bxljoy <259500313@qq.com>
2024-05-25 11:55:41 +0200

    feat: Add route for creating new note and update Note model

--- app/api/note/new/route.ts
+++ app/api/note/new/route.ts
@@ -7,18 +7,23 @@ interface RequestData {
   userId: string;
 }
 
-export async function POST(request: Request): Promise<void> {
+export async function POST(request: Request): Promise<Response> {
   const data = (await request.json()) as RequestData;
   const { note, tag, userId } = data;
 
   try {
     await connectDatabase();
 
-    await Note.create({
+    const newNote = new Note({
+      creator: userId,
       note,
       tag,
     });
+    await newNote.save();
+
+    return new Response(JSON.stringify(newNote), { status: 201 });
   } catch (error) {
     console.error(error);
+    return new Response("Error creating note", { status: 500 });
   }
 }
