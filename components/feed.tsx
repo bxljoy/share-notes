6f75b5c64c8a337b5198a999a772b2aaac03e842
bxljoy <259500313@qq.com>
2024-05-25 22:20:56 +0200

    feat: Add user authentication check before creating a new note

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import NoteCard from "./note-card";
+import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
+
 export default function Feed() {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
   return (
-    <section className="w-full flex-center flex-col">
-      <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input"
+        />
+      </form>
     </section>
   );
 }
