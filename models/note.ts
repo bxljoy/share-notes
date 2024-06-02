8aeae2e52609a210941c0fe7f58e9936bea7e0d5
bxljoy <259500313@qq.com>
2024-06-02 02:10:31 +0200

    refactor: Update note model to use imported User model

--- models/note.ts
+++ models/note.ts
@@ -1,8 +1,9 @@
 import { model, models, Schema, Model } from "mongoose";
 import { INote } from "@/interfaces/note";
+import User from "@/models/user";
 
 const NoteSchema = new Schema<INote>({
-  creator: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  creator: { type: Schema.Types.ObjectId, ref: User, required: true },
   note: { type: String, required: [true, "Note is required."] },
   tag: { type: String, required: [true, "Tag is required."] },
 });
