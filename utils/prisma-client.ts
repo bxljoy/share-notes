0e32dfc6477b0e3dcbeaa2433f8a837944092c61
bxljoy <259500313@qq.com>
2024-06-15 21:13:12 +0200

    chore: Update MongoDB connection URL and Prisma dependencies

--- utils/prisma-client.ts
+++ utils/prisma-client.ts
@@ -1,5 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+// const prisma = new PrismaClient();
 
 export default prisma;
